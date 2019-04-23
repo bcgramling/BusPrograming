f7e4ed200c551d0b18e65232f646e06bb1429130
Bryce Gramling <Brycegramling@uiowq.edu>
2019-04-23 16:53:35 -0500

    Finished HW4

--- HW4/src/Test.java
+++ HW4/src/Test.java
@@ -2,15 +2,15 @@ public class Test {
 public static void main(String[] args) {
 
 	//java -cp bin;stdlib.jar Test < data.txt > results.txt
-		int length_of_array = StdIn.readInt();
+		int length_of_array = StdIn.readInt();						//read in the length of the array from the input file
 
-		double [] array = new double [length_of_array];
-		for (int i =0; i< length_of_array;i++ ) {
+		double [] array = new double [length_of_array];				//create new array and set the length of the array
+		for (int i =0; i< length_of_array;i++ ) {					//put in the values from the text file into the array
 			array[i]=StdIn.readDouble();
 		}		
 				
-		System.out.printf("Standard Diviation: %.2f%n",(Utilities.Array_StdDiv(array)));
-		System.out.printf("Average: %.2f%n" , (Utilities.Array_Avg(array)));
+		System.out.printf("Standard Diviation: %.2f%n",(Utilities.Array_StdDiv(array)));	//print the std div using the std div function in the utilities file
+		System.out.printf("Average: %.2f%n" , (Utilities.Array_Avg(array)));				//print the average using the average function in the utilities file
 				
 		}
 	
--- HW4/src/Utilities.java
+++ HW4/src/Utilities.java
@@ -1,28 +1,27 @@
 public class Utilities {
 
-	public static double Array_Avg (double[] array) {
+	public static double Array_Avg (double[] array) {			//set up a new array used only for average. Pull in double from main function
 		double array_sum=0;
 		for (int i=0; i<array.length; i++) {
-			array_sum=array_sum+array[i];
+			array_sum=array_sum+array[i];						//sets sum of array
 		}
-		double array_avg=array_sum/array.length;
+		double array_avg=array_sum/array.length;				//calculated average of array
 		
-		return array_avg;
+		return array_avg;										//returns average to other functions
 	}
 
 		
-	public static double Array_StdDiv (double[] array)	{
-		double summation = 0;
+	public static double Array_StdDiv (double[] array)	{		//set up a new array used only for std div
+		double summation = 0;									//a new variable to keep track of sum of the summation part of the std div equation
 		
 		for (int j=0; j<array.length; j++) {
-			summation= summation + Math.pow((array[j]- Utilities.Array_Avg(array)),2);
+			summation= summation + Math.pow((array[j]- Utilities.Array_Avg(array)),2); //calculating summation for each number in array
 		}
-		//double array_StdDiv = summation;
 		
-		double array_StdDiv = Math.sqrt((summation /(array.length-1)));
-		
-		return array_StdDiv;
-	}
+		double array_StdDiv = Math.sqrt((summation /(array.length-1)));					//actually calculated std div of the array of numbers
+			
+		return array_StdDiv;															//returns std div of array to other functions
+	}	
 			
 				
 }
